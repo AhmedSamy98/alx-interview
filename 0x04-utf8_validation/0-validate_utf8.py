9c66f0242da3fd17ec4d03fd61877a7ff1342ac5
AhmedSamy98 <ahmed1samy1998@gmail.com>
2024-05-10 03:57:37 +0300

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python3
 """
-UTF-8 Validation
+    This Module Contains a funcion to validate utf-8
 """
 
 
 def validUTF8(data):
     """
-    data: a list of integers
-    Return: True if data is a valid UTF-8
-    encoding, else return False
+    This funciton validates utf-8
     """
-    byte_count = 0
+    def is_start_byte(byte):
+        return (byte & 0b10000000) == 0b00000000
 
-    for i in data:
-        if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_count = 1
-            elif i >> 4 == 0b1110:
-                byte_count = 2
-            elif i >> 3 == 0b11110:
-                byte_count = 3
-            elif i >> 7 == 0b1:
-                return False
+    def count_leading_ones(byte):
+        count = 0
+        mask = 0b10000000
+        while (byte & mask) == mask:
+            count += 1
+            mask >>= 1
+        return count
+
+    i = 0
+    while i < len(data):
+        leading_ones = count_leading_ones(data[i])
+        if leading_ones == 0:
+            i += 1
         else:
-            if i >> 6 != 0b10:
+            if leading_ones == 1 or leading_ones > 4 or i + \
+                    leading_ones > len(data):
                 return False
-            byte_count -= 1
-    return byte_count == 0
+
+            for j in range(1, leading_ones):
+                if not (data[i + j] & 0b11000000) == 0b10000000:
+                    return False
+
+            i += leading_ones
+
+    return True
